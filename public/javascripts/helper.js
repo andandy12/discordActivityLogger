63bc03525ba2f4ababc498ac96b8600885290a5d
andandy12 <30943710+andandy12@users.noreply.github.com>
2021-09-19 15:28:22 -0400

    more highcharts polish

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -43,11 +43,9 @@ var getData = async (id) => {
 
 var parseData = function (result) {
     let data = [[], [], [], []];
-    let timelength = document.getElementById("timeSelector").value;
-    let timetext = document.getElementById("timeSelector")[document.getElementById("timeSelector").options.selectedIndex].innerText;
     for (let i = 0; i < result.data.time.length; i++) {
         if (result.data.time[i - 1] && (result.data.time[i] - result.data.time[i - 1]) > 120000) {
-            //console.log("we may want to play a null here");
+            console.log("we may want to play a null here");
             data[0].push([result.data.time[i], null]);
             data[1].push([result.data.time[i], null]);
             data[2].push([result.data.time[i], null]);
@@ -56,36 +54,106 @@ var parseData = function (result) {
         data[1].push([result.data.time[i], result.data.active[i]]);
         data[2].push([result.data.time[i], result.data.active[i] / result.data.total[i] * 100]);
     }
-    while (data[0][data[0].length - 1][0] - data[0][0][0] > timelength) {
-        console.log(`Shifting due to length over ${timetext}`);
-        data[0].shift(); data[1].shift(); data[2].shift();
-
-    }
-    data[0].unshift([Date.now() - timelength, null]); // we want to force show the period of time we want to display
-    data[0].push([Date.now(), null]);
+    //data[0].unshift([Date.now() - timelength, null]); // we want to force show the period of time we want to display
+    //data[0].push([Date.now(), null]);
     data[3] = result.guild;
     return data;
 };
 let data = async () => {
     let data = parseData(await getData(document.getElementById("serverSelector").value));
     //console.log(data);
-    chart = Highcharts.chart('container', {
-
+    Highcharts.stockChart('container', {
         chart: {
-            zoomType: 'x'
+            plotBackgroundColor: 'hsl(200,4%,98%)'
+        },
+        credits: {
+            text: "hwidspoofer.com",
+            href: "https://hwidspoofer.com"
+        },
+        scrollbar: {
+            enabled : false
+        },
+        exporting:{
+            buttons:{
+                contextButton:{
+                    x: 5,
+                    y: -5,
+                }
+            },
+            printMaxWidth: 1560, // default times 2
+            sourceWidth: 1200,
+        },
+
+        rangeSelector: {
+            selected: 1,  
+            buttons: [{
+                type: 'day',
+                text: '1d',
+                title: 'View 1 day'
+            }, {
+                type: 'day',
+                count: 2,
+                text: '2d',
+                title: 'View 2 day'
+            }, {
+                type: 'week',
+                text: '1w',
+                title: 'View 1 week'
+            }, {
+                type: 'month',
+                text: '1m',
+                title: 'View 1 month'
+            },{
+                type: 'month',
+                count: 3,
+                text: '3m',
+                title: 'View 3 month'
+            }, {
+                type: 'all',
+                text: 'All',
+                title: 'View all'
+            }],
+            height:30,
+            //floating:true,
+
+        },
+        legend: {
+            enabled: true,
+            align: 'left',
+            layout: 'proximate',
+            //verticalAlign: 'top',
+            margin:-90,
+            padding:0,
+            symbolWidth: 0,
+            y: 12,
+            itemStyle: {
+                fontWeight: "normal",
+                color: '#666666',
+                opacity: "50%",
+            }
         },
 
         title: {
             text: data[3].name,
+            floating:false,
+            //y:45,
+            margin:-30,
         },
 
         subtitle: {
             text: data[3].id,
+            floating:true,
+            y:25
+
         },
 
         tooltip: {
             shared: true
         },
+
+        colors: [
+            ' rgba(236, 76, 145,0.99)','rgba(0, 160, 175,0.99)','rgba(255, 159, 152,0.99)', //'hsla(280,100%,80%,100%)','hsla(200,100%,80%,100%)',"hsla(240,100%,80%,100%)",
+        ],
 
         xAxis: {
             type: 'datetime',
@@ -95,44 +163,64 @@ let data = async () => {
         yAxis: [{ // Primary yAxis
             title: {
                 text: 'People',
+                margin: 5,
             },
             min: 0,
-
-
+            tickAmount:5,
+            labels:{
+                style: {
+                    opacity:"50%"
+                }
+            }
+            
         }, { // Secondary yAxis
-            gridLineWidth: 0,
-            title: {
-                text: 'Percentage',
-            },
-            labels: {
-                format: '{value} %',
-            },
-            opposite: true,
+            gridLineWidth: 2,
             max: 100,
             min: 0,
+            tickAmount:5,
+            alignTicks: true,
+            opposite: false,
+            labels: {
+                format: '{value}%',
+                align: 'center',
+                x:30,
+                style: {
+                    opacity:"50%"
+                }
+            },
+            title: {
+                reserveSpace: false,
+            }
 
         }],
+        plotOptions: {
+            series: {
+                showInNavigator: true,
+                lineWidth: 1,
+                connectNulls: false,
+                gapSize: 2,
+                opacity: 2,
+                showInLegend: true,
+            }
+        },
 
         series: [{
             data: data[0],
-            lineWidth: 0.5,
-            name: 'Total'
-        }, {
-            data: data[1],
-            lineWidth: 0.5,
-            name: 'Active',
-        }, {
+            name: 'Total',
+            
+        },{
             data: data[2],
-            lineWidth: 0.5,
             name: 'Percentage',
             yAxis: 1,
             tooltip: {
                 valueSuffix: ' %',
                 valueDecimals: 2,
             }
-        }]
+        }, {
+            data: data[1],
+            name: 'Active',
+        }, ]
 
     });
+    data = null; // we dont need to store this as the chart will make a copy
 };
-setInterval(data(), 120000);
-            //console.log(data);
